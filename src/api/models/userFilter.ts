3a5f6b4e9af8a886cc16b7a5b38072c2492e51c6
jacekroszkowiak <jacekroszkowiakdev@gmail.com>
2025-09-30 19:17:04 +0200

    feat(filters): expand supported operators for enhanced filtering

--- src/api/models/userFilter.ts
+++ src/api/models/userFilter.ts
@@ -3,27 +3,31 @@ import { Role } from '../../lib/types.js';
 
 
 export class UserFilter {
-  @Filterable(['eq', 'neq'], 'uuid')
+  @Filterable(['eq', 'neq', 'in', 'is_null', 'is_not_null'], 'uuid')
   id!: string;
 
-  @Filterable(['eq', 'contains'], 'string')
+  @Filterable(['eq', 'neq', 'contains', 'starts_with', 'ends_with', 'in', 'is_null', 'is_not_null'], 'string')
   email!: string;
 
-  @Filterable(['eq', 'neq', 'gt', 'lt', 'gte', 'lte', 'between'], 'number')
+  @Filterable(['eq', 'neq', 'contains', 'starts_with', 'ends_with', 'is_null', 'is_not_null'], 'string')
+  name!: string;
+
+  @Filterable(['eq', 'neq', 'gt', 'lt', 'gte', 'lte', 'between', 'in', 'is_null', 'is_not_null'], 'number')
   age!: number;
 
-  @Filterable(['eq', 'neq'], 'string')
+  @Filterable(['eq', 'neq', 'in', 'is_null', 'is_not_null'], 'string')
   role!: Role;
 
-  @Filterable(['eq', 'neq'], 'boolean')
+  @Filterable(['eq', 'neq', 'is_null', 'is_not_null'], 'boolean')
   isActive!: boolean;
 
-  @Filterable(['gt', 'lt', 'between'], 'date')
+  @Filterable(['eq', 'neq', 'gt', 'lt', 'gte', 'lte', 'between', 'is_null', 'is_not_null'], 'date')
   createdAt!: Date;
 
-  @Filterable(['gt', 'lt', 'between'], 'date')
+  @Filterable(['eq', 'neq', 'gt', 'lt', 'gte', 'lte', 'between', 'is_null', 'is_not_null'], 'date')
   joinDate!: Date;
 
+  @Filterable(['eq', 'neq', 'gt', 'lt', 'gte', 'lte', 'between', 'is_null', 'is_not_null'], 'date')
   updatedAt!: Date;
 
   constructor(data: Partial<UserFilter> = {}) {
